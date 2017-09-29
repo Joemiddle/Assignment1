9b3788ed124b9578aae5d7dcbfb100bf86657529
Joemiddle <andrewjm25@hotmail.com>
2017-09-29 11:06:58 -0400

    Assignment1

---
+++ Andrew/src/main/java/com/example/andrewassign1/AndrewActivity.java
@@ -0,0 +1,16 @@
+// Andrew McGuire n01121995 Assignment 1
+package com.example.andrewassign1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndrewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_andrew);
+
+
+    }
+}
